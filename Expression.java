9bcbec63fa84981f5d71f4fe382fc84231ffdd94
ncahlander <nk.cahlander@gmail.com>
2015-11-11 22:18:04 -0800

    Update Expression.java

--- Expression.java
+++ Expression.java
@@ -50,6 +50,26 @@ public class Expression implements Evaluable{
         //return 1.0;
 	}
 
+//tree stack 
+//1. make a stack of the trees by making new root every time we come across an operand -- fill RIGHT TO LEFT 
+//2. pop off top of stack (should be root operand)
+//3. loop/recursion to check right -- within each tree, check to see if we have filled right and left, stitch if there's empty - when we hit bottom, go out of right loop
+//4. loop/recursion to check lefted -- within each tree, check to see if we have filled the right and left 
+//done with if/ else if -- done!!!!! return full tree stitched together?
+
+//some code to ponder below~~~
+//get top
+//get next
+	// put on right
+//getnext
+	//put on right
+	//if full
+		//put on left
+//getnext
+	//put on right
+	//if full
+		//put on left
+		//if full go up?
     public void makeNode(String s)
     {
         //postfix into node, push node onto estack
