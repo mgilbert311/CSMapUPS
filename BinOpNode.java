20b8091aa5e6f4e7409559fb417e9df5043b2ed6
mgilbert311 <mgilbert311@gmail.com>
2015-11-15 14:57:48 -0800

    Update BinOpNode.java

--- BinOpNode.java
+++ BinOpNode.java
@@ -7,115 +7,98 @@ import java.util.Map;
  * @authorNicole Cahlander, Shannon Leahy, Mark Gilbert
  */
 public class BinOpNode implements Evaluable{
-    private String data;
-    static Evaluable leftNode;
-    static Evaluable rightNode;
-    Expression expr;
-        private final String NODE_TYPE = "BinOpNode";
+	private String data;
+	public static Evaluable leftTree;
+	public static Evaluable rightTree;
+	Expression expr;
+	BinOpNode bon;
 
-    
-    //Hello friends
-    //Im Here
-    /**
-     * 
-     * @param Op the operand used for evaluation
-     */
-    public BinOpNode (String Op, Evaluable left, Evaluable right){    
-    	   if(Op.equals("*")||Op.equals("/")||Op.equals("+")||Op.equals("-")){
-               this.data = Op;
-           } else{
-        	   throw new InputMismatchException("Input is not valid");
-           }
-    	    this.leftNode = left;
-            this.rightNode = right;
-    }
-    
-    /**
-     * 
-     * @param l
-     */
-    public void setLeft(Evaluable l) {
-        leftNode = l;
-    }
-    
-    /**
-     * 
-     * @param r
-     */
-    public void setRight(Evaluable r){
-        rightNode = r;
-    }
-    
-    /**
-     * 
-     * @return
-     */
-    public String getData(){
-        return data;
-    }
-    
-    /**Evaluates the expression and returns its value as a double.
+	/**
+	 * 
+	 * @param Op the operand used for evaluation
+	 */
+	public BinOpNode (String Op, Evaluable left, Evaluable right){    
+		if(Op.equals("*")||Op.equals("/")||Op.equals("+")||Op.equals("-")){
+			this.data = Op;
+			this.leftTree = left;
+			this.rightTree = right;
+		} else{
+			throw new InputMismatchException("Input is not valid");
+		}
+
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public String getData(){
+		return data;
+	}
+
+	/**Evaluates the expression and returns its value as a double.
        @param java.util.Map<java.lang.String,java.lang.Double>
        @return value*/
-    public double evaluate(Map<String, Double> env){
-    	 //         double leftValue = this.left.evaluate();
-        //         double rightValue = this.right.evaluate();
-        double leftValue = 1; //testing
-        double rightValue = 0; //testing
+	public double evaluate(Map<String, Double> env){
+		double fValue = 0;
+//		double left = leftTree.evaluate(env);
+//		double right = rightTree.evaluate(env);
+		double left;
+		double right;
+		//If the trees have
+		if(leftTree.equals(bon) && rightTree.equals(bon)){
+			left = leftTree.evaluate(env);
+			right = rightTree.evaluate(env);
+		}
+		if(data.equals("+")){
+			 left = leftTree.evaluate(env);
+			 right = rightTree.evaluate(env);
+			fValue = left + right;
+		}else if(data.equals("-")){
+			 left = leftTree.evaluate(env);
+			 right = rightTree.evaluate(env);
+			fValue = left - right;
+		}else if(data.equals("*")){
+			 left = leftTree.evaluate(env);
+			 right = rightTree.evaluate(env);
+			fValue = left * right;
+		}else if(data.equals("/")){
+			 left = leftTree.evaluate(env);
+			 right = rightTree.evaluate(env);
+			fValue = left / right;
+		}else{
+			System.err.println("Unknown operator: "+data.toString());
+		}
+    return fValue;
+	}
+	
+	/* How if a node only has it's one data value supposed to create an 
+    expression or do more then return it's data*/
+	/**
+	 * 
+	 */
 
-        double value = 0;
-        switch(data.substring(0)) {
-            case "+":
-            value = leftValue + rightValue;
-            break;
-            case "-":
-            value = leftValue - rightValue;
-            break;
-            case "*":
-            value = leftValue * rightValue;
-            break;
-            case "/":
-            value = leftValue / rightValue;
-            break;
-            default:
-            System.err.println("Unknown operator: "+data.toString());
-        }
-        return value;
+	public String toString(){
+		String binOp = "";
+		if(this == null){
+			return null; 
+		}else{
 
-    }
-    
-        public String getNODE_TYPE()
-    {
-        return NODE_TYPE;
-    }
-    
-    /* How if a node only has it's one data value supposed to create an 
-       expression or do more then return it's data*/
-    /**
-     * 
-     */
-    public String toString(){
-    	if(expr == null){
-    		return "( ";
-    	}
-    	else{
-    		expr.
-    	}
-//       if(leftNode == null && rightNode == null){
-//    	   return " ( " + data + " ) ";
-//       }else{
-//    	   System.out.println(" (");
-//    	   leftNode.toString();
-//    	   System.out.print(data);
-//    	   rightNode.toString();
-//    	   System.out.print(data);
-//    	   System.out.print(") ");
-//       }
-       return data;
-    }
-    
-    public static void main(String[] args){
-    	BinOpNode bin = new BinOpNode("*", leftNode, rightNode);
-    	System.out.println(bin.toString());
-    }
+			binOp = binOp + " (";
+			String left = leftTree.toString();
+			binOp = binOp + " " + left;
+
+			binOp = binOp + " " + data + " ";
+
+			String right = rightTree.toString();
+			binOp = binOp + right;
+			binOp = binOp +" ) ";
+		}
+
+		return binOp;
+	}
+
+	public static void main(String[] args){
+		//BinOpNode bin = new BinOpNode("*", leftTree, rightTree);
+		//System.out.println(bin.toString());
+	}
 }
